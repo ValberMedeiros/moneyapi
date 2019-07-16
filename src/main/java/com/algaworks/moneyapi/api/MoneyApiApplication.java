339ae7900b7cadde0df90d09046964cb9191042e
valberjunior <dev.junior.medeiros@gmail.com>
2019-07-15 23:56:32 -0300

    initial commit

---
+++ src/main/java/com/algaworks/moneyapi/api/MoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.moneyapi.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/algaworks/moneyapi/api/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.algaworks.moneyapi.api;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MoneyApiApplication.class);
+    }
+
+}
