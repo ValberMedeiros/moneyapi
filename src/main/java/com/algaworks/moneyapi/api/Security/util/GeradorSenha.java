038a552f2fcbf18c25a3b219935fc916244537e1
valberjunior <dev.junior.medeiros@gmail.com>
2019-07-28 17:31:11 -0300

    Implementado model de usuarios e permissoes

---
+++ src/main/java/com/algaworks/moneyapi/api/Security/util/GeradorSenha.java
@@ -0,0 +1,10 @@
+package com.algaworks.moneyapi.api.Security.util;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class GeradorSenha {
+    public static void main(String[] args) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        System.out.println(encoder.encode("@ngul@r0"));
+    }
+}
--- src/main/java/com/algaworks/moneyapi/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/moneyapi/api/config/AuthorizationServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -19,14 +20,17 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Autowired
+    private UserDetailsService userDetailsService;
+
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
         clients.inMemory()
                 .withClient("angular")
-                .secret("@ngul@r0")
+                .secret("$2a$10$X10pw3oymAlXqSOoOHGblON.98TMWBnPJscWLJqgWC2sODyycMa3O")
                 .scopes("read", "write")
                 .authorizedGrantTypes("password", "refresh_token")
-                .accessTokenValiditySeconds(20)
+                .accessTokenValiditySeconds(1800)
                 .refreshTokenValiditySeconds(3600 * 24);
     }
 
@@ -36,7 +40,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .tokenStore(tokenStore())
                 .accessTokenConverter(accessTokenConverter())
                 .reuseRefreshTokens(false)
-                .authenticationManager(authenticationManager);
+                .userDetailsService(this.userDetailsService)
+                .authenticationManager(this.authenticationManager);
     }
 
     @Bean
